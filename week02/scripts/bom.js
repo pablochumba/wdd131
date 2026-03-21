0b3e1ede0b6cc7ae78229159d45f6ea9e2dce113
pablochumba <pablo.chumba79@gmail.com>
2026-03-21 19:06:44 -0300

    finished functional bom thing

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,19 +2,22 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-deleteButton.setAttribute('aria-label', 'Delete');
+button.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        deleteButton.setAttribute('aria-label', 'Delete');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.value = '';
+        input.focus();
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-
-li.appendChild(deleteButton);
-list.appendChild(li);
-
-
-
-
-
-
+        deleteButton.addEventListener('click', (event) => {
+            deleteButton.parentNode.remove();
+            input.focus();
+        });
+    }
+});
 
